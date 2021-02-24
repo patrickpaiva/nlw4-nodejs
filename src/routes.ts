cc9a2f90b7dbb4a974c5fa4e07d6445338c58b54
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-24 16:20:15 -0300

    created show method and route to list all Surveys

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ const userController = new UserController()
 const surveyController = new SurveysController()
 
 router.post("/users", userController.create)
+
+router.get("/surveys", surveyController.show)
 router.post("/surveys", surveyController.create)
 
 export { router }

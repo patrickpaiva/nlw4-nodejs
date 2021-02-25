cfb5c6c0bbec53252048de8351cc70e1cbde8a8d
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-25 20:20:12 -0300

    added relations between entities

--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -1,5 +1,7 @@
 import {v4 as uuid} from 'uuid'
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { User } from './User';
+import { Survey } from './Survey';
 
 @Entity("surveys_users")
 class SurveyUser {
@@ -8,10 +10,18 @@ class SurveyUser {
 
     @Column()
     user_id: string
-    
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: "user_id"})
+    user: User
+
     @Column()
     survey_id: string
-
+    
+    @ManyToOne(() => Survey)
+    @JoinColumn({name: "survey_id"})
+    survey: Survey
+    
     @Column()
     value: number
 

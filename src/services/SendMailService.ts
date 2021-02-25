f95fbd9d9922236947f6b86361aa8c60e2902e40
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-25 19:53:06 -0300

    created email template with hbs

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,6 @@
 import nodemailer, { Transporter } from 'nodemailer'
+import handlebars from 'handlebars'
+import fs from 'fs'
 
 class SendMailService {
     private client: Transporter
@@ -17,11 +19,16 @@ class SendMailService {
         })
 
     }
-    async execute(to: string, subject: string, body: string) {
+    async execute(to: string, subject: string, variables: object, path: string) {
+        const templateFileContent = fs.readFileSync(path).toString('utf8')
+
+        const mailTemplateParsed = handlebars.compile(templateFileContent)
+        const html = mailTemplateParsed(variables)
+
         const message = await this.client.sendMail({
             to,
             subject,
-            html: body,
+            html,
             from: 'NPS <noreply@nps.com.br>'
         })
 

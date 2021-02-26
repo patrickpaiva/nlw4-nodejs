d0034cfd23b76b406ccd7635c2c5170a15bcd3ef
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-26 12:09:01 -0300

    added logic to retrieve users Answers

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import { UserController } from './controllers/UserController'
 import { SurveysController } from './controllers/SurveysController'
 import { SendMailController } from './controllers/SendMailController'
+import { AnswerController } from './controllers/AnswerController'
 
 const router = Router()
 
 const userController = new UserController()
 const surveyController = new SurveysController()
 const sendMailController = new SendMailController()
+const answerController = new AnswerController()
 
 router.post("/users", userController.create)
 
@@ -15,5 +17,7 @@ router.get("/surveys", surveyController.show)
 router.post("/surveys", surveyController.create)
 
 router.post("/sendMail", sendMailController.execute)
+
+router.get("/answers/:value", answerController.execute)
 
 export { router }

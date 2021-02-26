2ac5504b865655892983378b72e76bdbcdb79925
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-26 12:26:47 -0300

    added feature to calculate NPS

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { UserController } from './controllers/UserController'
 import { SurveysController } from './controllers/SurveysController'
 import { SendMailController } from './controllers/SendMailController'
 import { AnswerController } from './controllers/AnswerController'
+import { NpsController } from './controllers/NPSController'
 
 const router = Router()
 
@@ -10,6 +11,7 @@ const userController = new UserController()
 const surveyController = new SurveysController()
 const sendMailController = new SendMailController()
 const answerController = new AnswerController()
+const npsController = new NpsController()
 
 router.post("/users", userController.create)
 
@@ -19,5 +21,7 @@ router.post("/surveys", surveyController.create)
 router.post("/sendMail", sendMailController.execute)
 
 router.get("/answers/:value", answerController.execute)
+
+router.get("/nps/:survey_id", npsController.execute)
 
 export { router }
